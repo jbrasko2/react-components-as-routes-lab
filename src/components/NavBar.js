a2c73a6eb008657f16eaa3676494c2372f539adb
Jordan Brasko <jbrasko0508@gmail.com>
2021-03-05 10:30:59 -0800

    commit tree 9fbeb9058124b268d5a7d76f0a65d9814c4e9665

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,35 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        activeStyle={{
+          color: 'darkblue'
+        }}
+      >Home</NavLink>
+      <NavLink
+      to="/movies"
+      exact
+      activeStyle={{
+        color: 'darkblue'
+      }}
+      >Movies</NavLink>
+      <NavLink
+      to="/directors"
+      exact
+      activeStyle={{
+        color: 'darkblue'
+      }}
+      >Directors</NavLink>
+      <NavLink
+      to="/actors"
+      exact
+      activeStyle={{
+        color: 'darkblue'
+      }}
+      >Actors</NavLink>
     </div>
   );
 };
